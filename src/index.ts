57e2a40d991bf7648ee0a2d0993a04dc0d89f4e8
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-10-19 17:59:34 -0400

    basic running setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Telegraf from 'telegraf';
 import {APIGatewayProxyHandler, APIGatewayProxyEvent} from 'aws-lambda';
+import {config} from 'dotenv';
 
 const bot = new Telegraf(process.env.BOT_TOKEN!, {
 });
@@ -9,13 +10,16 @@ bot.start((ctx) => {
   ctx.reply('Welcome');
 });
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.command('login', (ctx) => {
-
-// });
+bot.command('webhook', (ctx) => {
+  const chatId = ctx.chat!.id;
+  ctx.replyWithMarkdown(`Please use this webhook url:
+${5}/developer_webhook_bot-hook?chatid=${encodeURIComponent(chatId)}
+  Content-Type: application/json
+  Secret: \`\`
+`);
+});
 
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 
 function getBody(event: APIGatewayProxyEvent): any {
@@ -28,18 +32,40 @@ function getBody(event: APIGatewayProxyEvent): any {
   return JSON.parse(event.body);
 }
 
-export const botbook: APIGatewayProxyHandler = async (event, context) => {
+function badRequest() {
+  return {statusCode: 400, body: JSON.stringify('Bad Request')};
+}
+
+function ok() {
+  return {statusCode: 200, body: JSON.stringify('Ok')};
+}
+
+export const bothook: APIGatewayProxyHandler = async (event, context) => {
   console.log(event, context);
   if (!event.body || event.body.length === 0) {
-    return {statusCode: 400, body: 'Bad Request'};
+    return badRequest();
   }
   const body = getBody(event);
   await bot.handleUpdate(body);
-  return {statusCode: 200, body: 'Ok'};
+  return ok();
 };
 
 export const webhook: APIGatewayProxyHandler = async (event, context) => {
   console.log(event, context);
-  //const body = getBody(event);
-  return {statusCode: 200, body: 'Ok'};
+  if (!event.body || event.body.length === 0) {
+    return badRequest();
+  }
+  if (!event.queryStringParameters || !event.queryStringParameters.chatid) {
+    return badRequest();
+  }
+  const body = getBody(event);
+  const chatId = decodeURIComponent(event.queryStringParameters.chatid!);
+  await bot.telegram.sendMessage(chatId, JSON.stringify(body));
+  return ok();
 };
+
+if (require.main === module) {
+  config();
+  bot.token = process.env['developer_webhook_bot.BOT_TOKEN']!;
+  bot.launch();
+}
