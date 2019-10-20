e5ad092a90dab2a13fd6928e3f448e4bacbc3f95
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-10-20 14:55:58 -0400

    add more github notifcations

--- api/webhooks/github/[chatid].ts
+++ api/webhooks/github/[chatid].ts
@@ -1,10 +1,13 @@
 import {NowRequest, NowResponse} from '@now/node';
-import WebhooksApi, {WebhookPayloadIssueComment, WebhookPayloadIssues, WebhookPayloadIssuesIssueUser, WebhookPayloadPullRequest, WebhookPayloadPullRequestReview, WebhookPayloadPullRequestReviewComment} from '@octokit/webhooks';
+import WebhooksApi, {WebhookPayloadIssueComment, WebhookPayloadIssues, WebhookPayloadIssuesIssueUser, WebhookPayloadPullRequest, WebhookPayloadPullRequestReview, WebhookPayloadPullRequestReviewComment, WebhookPayloadCommitComment, WebhookPayloadRelease, WebhookEvent, WebhookPayloadRepository, PayloadRepository, WebhookPayloadStatus} from '@octokit/webhooks';
 import {createSecret} from '../../_internal/secret';
 import {replyer} from '../../_internal/telegram';
 import {ok} from '../../_internal/responses';
 
-function link(url: string, title: string) {
+function link(url: string | null, title: string) {
+  if (!url) {
+    return title;
+  }
   return `[${title}](${url})`;
 }
 
@@ -18,10 +21,33 @@ function issueLink(payload: WebhookPayloadIssues) {
   return link(issue.html_url, `${repo.full_name}#${issue.number} ${issue.title}`);
 }
 
+function releaseLink(payload: WebhookPayloadRelease) {
+  const release = payload.release;
+  const repo = payload.repository;
+  return link(release.html_url, `${repo.full_name} ${release.name}`);
+}
+
+function repoLink(payload: {repository: PayloadRepository}) {
+  const repo = payload.repository;
+  return link(repo.html_url, `${repo.full_name}`);
+}
+
 function commentLink(payload: WebhookPayloadIssueComment | WebhookPayloadPullRequestReviewComment) {
   const base = (payload as WebhookPayloadIssueComment).issue || (payload as WebhookPayloadPullRequestReviewComment).pull_request;
   const repo = payload.repository;
   return link(payload.comment.html_url, `${repo.full_name}#${base.number} ${base.title}`);
+}
+
+function commitCommentLink(payload: WebhookPayloadCommitComment) {
+  const base = payload.comment;
+  const repo = payload.repository;
+  return link(payload.comment.html_url, `${repo.full_name}@${base.commit_id}`);
+}
+
+function commitLink(payload: WebhookPayloadStatus) {
+  const base = payload.commit;
+  const repo = payload.repository;
+  return link(base.html_url, `${repo.full_name}@${base.sha.slice(0, 7)}}`);
 }
 
 function prLink(payload: WebhookPayloadPullRequest) {
@@ -44,30 +70,34 @@ function init(api: WebhooksApi, chatId: string) {
     return reply(`🐛 New issue ${issueLink(payload)}\nby ${user(payload.issue.user)}`, payload.issue.body);
   });
   api.on('issues.closed', async ({payload}) => {
-    return reply(`🐛 Issue ${issueLink(payload)} Closed\nby ${user(payload.issue.user)}`);
+    return reply(`🐛❌ Closed Issue ${issueLink(payload)}\nby ${user(payload.issue.user)}`);
   });
   api.on('issues.reopened', async ({payload}) => {
-    return reply(`🐛 Issue ${issueLink(payload)} Reopened\nby ${user(payload.issue.user)}`);
+    return reply(`🐛 Reopened Issue ${issueLink(payload)}\nby ${user(payload.issue.user)}`);
   });
 
   api.on('issue_comment.created', ({payload}) => {
     return reply(`💬 New comment on ${commentLink(payload)}\nby ${user(payload.comment.user)}`, payload.comment.body);
   });
   api.on('issue_comment.edited', ({payload}) => {
-    return reply(`💬 Comment on ${commentLink(payload)} Edited\nby ${user(payload.comment.user)}`, payload.comment.body);
+    return reply(`💬📝 Comment on ${commentLink(payload)} Edited\nby ${user(payload.comment.user)}`, payload.comment.body);
   });
   api.on('issue_comment.deleted', ({payload}) => {
-    return reply(`💬 Comment on ${commentLink(payload)} Deleted\nby ${user(payload.comment.user)}`);
+    return reply(`💬❌ Comment on ${commentLink(payload)} Deleted\nby ${user(payload.comment.user)}`);
   });
 
   api.on('pull_request.opened', ({payload}) => {
     return reply(`🔌 New pull request ${prLink(payload)}\nby ${user(payload.pull_request.user)}`);
   });
   api.on('pull_request.closed', ({payload}) => {
-    return reply(`🔌 Pull request ${prLink(payload)} Closed\nby ${user(payload.pull_request.user)}`);
+    if (payload.pull_request.merged) {
+      return reply(`🔌🥂 Merged & Closed Pull request ${prLink(payload)}\nby ${user(payload.pull_request.user)}`);
+    } else {
+      return reply(`🔌❌ Closed Pull request ${prLink(payload)}\nby ${user(payload.pull_request.user)}`);
+    }
   });
-  api.on('pull_request.closed', ({payload}) => {
-    return reply(`🔌 Pull request ${prLink(payload)} Closed\nby ${user(payload.pull_request.user)}`);
+  api.on('pull_request.ready_for_review', ({payload}) => {
+    return reply(`🔌⏳ Pull request ${prLink(payload)} Ready for Review`);
   });
 
   api.on('pull_request_review.submitted', ({payload}) => {
@@ -78,9 +108,9 @@ function init(api: WebhooksApi, chatId: string) {
       case 'approved':
         return reply(`✅ New pull request review ${reviewLink(payload)}\nApproved by ${user(review.user)}`, review.body);
       case 'request_changes':
-        return reply(`!! New pull request review ${reviewLink(payload)}\nRequest Changes by ${user(review.user)}`, review.body);
+        return reply(`‼ New pull request review ${reviewLink(payload)}\nRequest Changes by ${user(review.user)}`, review.body);
       default:
-        return reply(`New pull request review ${reviewLink(payload)}\nCommented by ${user(review.user)}`, review.body);
+        return reply(`❓ New pull request review ${reviewLink(payload)}\nCommented by ${user(review.user)}`, review.body);
     }
   });
 
@@ -103,6 +133,75 @@ function init(api: WebhooksApi, chatId: string) {
       body.push(`${link(commit.url, commit.id.slice(0, 7))}: ${commit.message} by ${commit.author.name}`);
     }
     return reply(header, body.join('\n'));
+  });
+
+  api.on('commit_comment.created', ({payload}) => {
+    const diff = `\`${payload.comment.path}\n${payload.comment.line}\``;
+    return reply(`💬 New commit comment on ${commitCommentLink(payload)}\nby ${user(payload.comment.user)}`, diff, payload.comment.body);
+  });
+
+  api.on('release.created', ({payload}) => {
+    let sub = ''
+    if (payload.release.draft) {
+      sub = 'draft ';
+    } else if (payload.release.prerelease) {
+      sub = 'pre';
+    }
+    return reply(`🎉 New ${sub}release ${releaseLink(payload)}\nby ${user(payload.release.author)}`, payload.release.body);
+  });
+
+  api.on('public', ({payload}) => {
+    return reply(`🥂 ${repoLink(payload)} was made public`);
+  });
+
+  api.on('repository_vulnerability_alert.create', ({payload}) => {
+    const alert = payload.alert;
+    return reply(`💊 Vulnerability Alert for ${repoLink(payload)} in \`${alert.affected_package_name}\``);
+  });
+
+  api.on('star.created', ({payload}) => {
+    return reply(`⭐ ${repoLink(payload)} was starred by ${user(payload.sender)}`);
+  });
+
+  api.on('fork', ({payload}) => {
+    return reply(`⭐ ${repoLink(payload)} was forked by ${user(payload.sender)}`);
+  });
+
+  api.on('deployment_status', ({payload}) => {
+    const status = payload.deployment_status;
+    switch (status.state) {
+      case 'success':
+        return reply(`☀ ${repoLink(payload)} was successfully ${link(status.deployment_url, 'deployed')}`, status.description);
+      case 'failure':
+        return reply(`🌩 ${repoLink(payload)} failed to ${link(status.deployment_url, 'deploy')}`, status.description);
+      case 'error':
+        return reply(`🌩 ${repoLink(payload)} errored while ${link(status.deployment_url, 'deploying')}`, status.description);
+    }
+    return undefined;
+  });
+
+  api.on('status', ({payload}) => {
+    switch (payload.state) {
+      case 'success':
+        return reply(`☀ Commit ${commitLink(payload)} state is ${link(payload.target_url, 'successful')}`, payload.description);
+      case 'failure':
+        return reply(`🌩 Commit ${commitLink(payload)} state is ${link(payload.target_url, 'failure')}`, payload.description);
+      case 'error':
+        return reply(`🌩 Commit ${commitLink(payload)} state is ${link(payload.target_url, 'error')}`, payload.description);
+    }
+    return undefined;
+  });
+
+  api.on('check_run.completed', ({payload}) => {
+    switch (payload.check_run.conclusion as unknown as string) {
+      case 'success':
+        return reply(`☀ Check Run ${payload.check_run.name} in ${repoLink(payload)} was a ${link(payload.check_run.html_url, 'success')}`, payload.check_run.output.summary);
+      case 'failure':
+        return reply(`🌩 Check Run ${payload.check_run.name} in ${repoLink(payload)} ${link(payload.check_run.html_url, 'failed')}`, payload.check_run.output.summary);
+      case 'action_required':
+        return reply(`🌩 Check Run ${payload.check_run.name} in ${repoLink(payload)} ${link(payload.check_run.html_url, 'requires action')}`, payload.check_run.output.summary);
+    }
+    return undefined;
   });
 
   // api.on('gollum', ({payload}) => {
