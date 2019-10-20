2f232286bd186dcdfb42a35a33ccf6f8cb162c66
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-10-19 21:26:17 -0400

    more github messages

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,16 @@ const baseSecret = process.env.WEBHOOK_SECRET || process.env['developer_webhook_
 export function createSecret(chatId: string) {
   return createHmac('sha1', chatId).update(Buffer.from(baseSecret)).digest('hex');
 }
+
+const DEFAULT_TRUNCATION_LIMIT = 4096;
+const TRUNCATED_MESSAGE = '**Truncated message, open on GitHub to read more**';
+
+export function truncateMessage(header: string, body: string, footer = '') {
+  const full = `${header}\n\n${body}\n${footer}`;
+  if (full.length < DEFAULT_TRUNCATION_LIMIT) {
+    return full;
+  }
+  const remaining = DEFAULT_TRUNCATION_LIMIT - header.length - footer.length - TRUNCATED_MESSAGE.length - 10;
+  return `${header}\n\n${body.slice(0, remaining)}\n${TRUNCATED_MESSAGE}\n${footer}`
+
+}
--- src/webhooks/github.ts
+++ src/webhooks/github.ts
@@ -1,14 +1,130 @@
-import WebhooksApi from '@octokit/webhooks';
+import WebhooksApi, {WebhookPayloadIssues, WebhookPayloadIssuesIssueUser, WebhookPayloadIssueComment, WebhookPayloadPullRequest, WebhookPayloadPullRequestReview, WebhookPayloadPullRequestReviewComment} from '@octokit/webhooks';
 import {Telegram} from 'telegraf';
-import {createSecret} from '../utils';
-import {APIGatewayProxyEvent} from 'aws-lambda';
-import {badRequest, ok, normalizeHeaders} from '../responses';
+import {createSecret, truncateMessage} from '../utils';
+import {badRequest, ok, normalizeHeaders, getBody} from '../responses';
 import {IWebHookHandler} from './interfaces';
+import {APIGatewayProxyEvent} from 'aws-lambda';
+
+function link(url: string, title: string) {
+  return `[${title}](${url})`;
+}
+
+function replyer(telegram: Telegram, chatId: string) {
+  return (header: string, body?: string | null, footer?: string) => {
+
+    const msg = body ? truncateMessage(header, body, footer) : truncateMessage('', header);
+
+    return telegram.sendMessage(chatId, msg, {
+      disable_web_page_preview: true,
+      parse_mode: 'Markdown'
+    }).then(() => undefined);
+  };
+}
+
+function user(user: WebhookPayloadIssuesIssueUser) {
+  return link(user.html_url, `@${user.login}`);
+}
+
+function issueLink(payload: WebhookPayloadIssues) {
+  const issue = payload.issue;
+  const repo = payload.repository;
+  return link(issue.html_url, `${repo.full_name}#${issue.number} ${issue.title}`);
+}
+function commentLink(payload: WebhookPayloadIssueComment | WebhookPayloadPullRequestReviewComment) {
+  const base = (payload as WebhookPayloadIssueComment).issue || (payload as WebhookPayloadPullRequestReviewComment).pull_request;
+  const repo = payload.repository;
+  return link(payload.comment.html_url, `${repo.full_name}#${base.number} ${base.title}`);
+}
+
+function prLink(payload: WebhookPayloadPullRequest) {
+  const pr = payload.pull_request;
+  const repo = payload.repository;
+  return link(pr.html_url, `${repo.full_name}#${pr.number} ${pr.title}`);
+}
+
+function reviewLink(payload: WebhookPayloadPullRequestReview) {
+  const review = payload.review;
+  const pr = payload.pull_request;
+  const repo = payload.repository;
+  return link(review.html_url, `${repo.full_name}#${pr.number} ${pr.title}`);
+}
 
 function init(api: WebhooksApi, chatId: string, telegram: Telegram) {
-  api.on('*', async (event) => {
-    await telegram.sendMessage(chatId, JSON.stringify(event.payload).slice(0, 300));
+  const reply = replyer(telegram, chatId);
+
+  api.on('issues.opened', async ({payload}) => {
+    return reply(`🐛 New issue ${issueLink(payload)}\nby ${user(payload.issue.user)}`, payload.issue.body);
   });
+  api.on('issues.closed', async ({payload}) => {
+    return reply(`🐛 Issue ${issueLink(payload)} Closed\nby ${user(payload.issue.user)}`);
+  });
+  api.on('issues.reopened', async ({payload}) => {
+    return reply(`🐛 Issue ${issueLink(payload)} Reopened\nby ${user(payload.issue.user)}`);
+  });
+
+  api.on('issue_comment.created', ({payload}) => {
+    return reply(`💬 New comment on ${commentLink(payload)}\nby ${user(payload.comment.user)}`, payload.comment.body);
+  });
+  api.on('issue_comment.edited', ({payload}) => {
+    return reply(`💬 Comment on ${commentLink(payload)} Edited\nby ${user(payload.comment.user)}`, payload.comment.body);
+  });
+  api.on('issue_comment.deleted', ({payload}) => {
+    return reply(`💬 Comment on ${commentLink(payload)} Deleted\nby ${user(payload.comment.user)}`);
+  });
+
+  api.on('pull_request.opened', ({payload}) => {
+    return reply(`🔌 New pull request ${prLink(payload)}\nby ${user(payload.pull_request.user)}`);
+  });
+  api.on('pull_request.closed', ({payload}) => {
+    return reply(`🔌 Pull request ${prLink(payload)} Closed\nby ${user(payload.pull_request.user)}`);
+  });
+  api.on('pull_request.closed', ({payload}) => {
+    return reply(`🔌 Pull request ${prLink(payload)} Closed\nby ${user(payload.pull_request.user)}`);
+  });
+
+  api.on('pull_request_review.submitted', ({payload}) => {
+    const review = payload.review;
+    switch (review.state) {
+      case 'commented':
+        return reply(`💬 New pull request review ${reviewLink(payload)}\nCommented by ${user(review.user)}`, review.body);
+      case 'approved':
+        return reply(`✅ New pull request review ${reviewLink(payload)}\nApproved by ${user(review.user)}`, review.body);
+      case 'request_changes':
+        return reply(`!! New pull request review ${reviewLink(payload)}\nRequest Changes by ${user(review.user)}`, review.body);
+      default:
+          return reply(`New pull request review ${reviewLink(payload)}\nCommented by ${user(review.user)}`, review.body);
+    }
+  });
+
+  api.on('pull_request_review_comment.created', ({payload}) => {
+    const diff = `\`${payload.comment.path}\n${payload.comment.diff_hunk}\``;
+    return reply(`💬 New pull request review comment ${commentLink(payload)}\nby ${user(payload.comment.user)}\n${diff}`, payload.comment.body);
+  });
+
+  api.on('push', ({payload}) => {
+    const commits = payload.commits;
+    const ref = payload.ref;
+    if (commits.length === 0 || !ref.startsWith('refs/head/')) {
+      return;
+    }
+    const branch = ref.substring('refs/head/'.length);
+
+    const header = `🔨 ${link(payload.compare, `${commits.length} new commits`)} to ${payload.repository.full_name}:${branch}`;
+    const body: string[] = [];
+    for (const commit of commits) {
+      body.push(`${link(commit.url, commit.id.slice(0, 7))}: ${commit.message} by ${commit.author.name}`);
+    }
+    return reply(header, body.join('\n'));
+  });
+
+  // api.on('gollum', ({payload}) => {
+  //   //  Wiki page is created or updated.
+
+  // });
+  // api.on('*', async (event) => {
+  //   console.log(event.name, event.payload.action);
+  //   await telegram.sendMessage(chatId, JSON.stringify(event.payload).slice(0, 300));
+  // });
 }
 
 export default class GithubWebHook implements IWebHookHandler {
@@ -46,7 +162,7 @@ export default class GithubWebHook implements IWebHookHandler {
       id: headers['x-request-id'],
       name: headers['x-github-event'],
       signature: headers['x-hub-signature'],
-      payload: event.body
+      payload: getBody(event)
     })
 
     return ok();
