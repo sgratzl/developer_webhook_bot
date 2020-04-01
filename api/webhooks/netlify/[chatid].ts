b7262674a1440e06e8e56badc9ca3e4e0b95e647
Samuel Gratzl <sam@sgratzl.com>
2020-04-01 15:27:44 +0200

    simple parsing of netlify messages

--- api/webhooks/netlify/[chatid].ts
+++ api/webhooks/netlify/[chatid].ts
@@ -1,11 +1,33 @@
-import {NowRequest, NowResponse} from '@now/node';
-import {ok} from '../../_internal/responses';
-import {replyer} from '../../_internal/telegram';
+import { NowRequest, NowResponse } from '@now/node';
+import { ok } from '../../_internal/responses';
+import { replyer } from '../../_internal/telegram';
 import { createSecret } from '../../_internal/secret';
 
 interface INetlifyMessage {
-  dummy: string;
+  name: string;
+  state: 'ready' | 'building' | 'failed';
+  buildid: string;
+  sslurl: string;
+  adminurl: string;
+  deploysslurl: string;
+
+  errormessage: null;
+
+  committer: string;
+  branch: string;
+
+  summary: {
+    status: 'ready';
+    messages: {
+      type: 'info';
+      title: 'string';
+      description: string;
+      details: string;
+    }[];
+  };
+  screenshoturl: string;
 }
+// https://app.netlify.com/sites/upsetjs/deploys/5e849340c9edf700064ebf61
 
 export const NAME = 'Netlify';
 
@@ -17,6 +39,13 @@ export function webhookMessage(server: string, chatId: string) {
   [${url}](${url})
   Secret: \`${secret}\`
   `;
+}
+
+function link(url: string | null, title: string) {
+  if (!url) {
+    return title;
+  }
+  return `[${title}](${url})`;
 }
 
 export default async function handle(req: NowRequest, res: NowResponse) {
@@ -32,7 +61,17 @@ export default async function handle(req: NowRequest, res: NowResponse) {
 
   const reply = replyer(chatId);
 
-  await reply(JSON.stringify(body));
+  const deploymentUrl = `${body.adminurl}/deploys/${body.buildid}`;
+  const url = link(body.sslurl, `Netlify App ${body.name}`);
+
+  switch (body.state) {
+    case 'ready':
+      await reply(`☀ ${url} was successfully ${link(deploymentUrl, 'deployed')}`);
+      break;
+    case 'failed':
+      await reply(`🌩 ${url} failed to ${link(deploymentUrl, 'deploy')}`);
+      break;
+  }
 
   return ok(res);
 }
